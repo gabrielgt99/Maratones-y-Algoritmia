63ef77f796822140cb2eb09eb373d35b4513f330
Gabriel González <gabrielgt99@gmail.com>
2021-01-09 14:53:51 -0500

    Simple Problem

---
+++ E-Olymp/SimpleProblem.cpp
@@ -0,0 +1,15 @@
+// Simple Problem?
+
+// https://www.e-olymp.com/en/problems/1
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num;
+  cin >> num;
+
+  cout << num/10 << " " << num%10 << '\n';
+  return 0;
+}
