58ee797f60236c0ba775f9a672fd3a6c0b023023
Gabriel González <gabrielgt99@gmail.com>
2021-01-18 23:39:38 -0500

    Elephant

---
+++ Codeforces/Elephant.cpp
@@ -0,0 +1,14 @@
+// Elephant
+
+// http://codeforces.com/contest/617/problem/A
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int fri;
+  cin >> fri;
+  cout << (fri+4)/5 << '\n';
+  return 0;
+}
